8d4f8cac5f5193a55a2c23dc949bf5c9859f76f7
suncruit <suncruit@gmail.com>
2023-03-18 14:07:19 +0900

    chore (config)
    - SpringBoot 2.7.9 -> 2.7.8
    - JPA @Query 에러

--- src/main/java/com/blog/blogsearch/BlogSearchApplication.java
+++ src/main/java/com/blog/blogsearch/BlogSearchApplication.java
@@ -2,7 +2,9 @@ package com.blog.blogsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BlogSearchApplication {
 
