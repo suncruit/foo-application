a0daf0ef650b03e11062bf340c8d7f95752ed296
suncruit <suncruit@gmail.com>
2023-03-19 17:59:42 +0900

    mod(json-parser)
    - JSONParserUtil 변환 클래스 제한

--- src/main/java/com/blog/blogsearch/common/utils/JSONParserUtil.java
+++ src/main/java/com/blog/blogsearch/common/utils/JSONParserUtil.java
@@ -2,13 +2,14 @@ package com.blog.blogsearch.common.utils;
 
 import com.blog.blogsearch.common.exception.RestAPIException;
 import com.blog.blogsearch.common.exception.code.CommonErrorCode;
+import org.json.simple.JSONAware;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class JSONParserUtil {
     private static final JSONParser parser = new JSONParser();
 
-    public static <T> T parseToJSON(String target, Class<T> responseType) {
+    public static <T extends JSONAware> T parseToJSON(String target, Class<T> responseType) {
         try {
             Object obj = parser.parse(target);
             return responseType.cast(obj);
